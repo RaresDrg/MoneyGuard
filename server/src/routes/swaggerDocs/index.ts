d1d458cb07f1daf300b0b46b80af02524b614f4a
RaresDrg <dragan_rares13@yahoo.com>
2026-01-04 07:38:59 +0200

    test asarsa
    satata

--- server/src/routes/swaggerDocs/index.ts
+++ server/src/routes/swaggerDocs/index.ts
@@ -4,8 +4,13 @@ import { IN_DEVELOPMENT } from "../../config/config-env.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const swaggerDocs = IN_DEVELOPMENT
+const swaggerDocs = [`${__dirname}/*.js`];
+
+const swaggerDocs2 = IN_DEVELOPMENT
   ? [`${__dirname}/*.ts`]
   : [`${__dirname}/*.js`];
+
+console.log(swaggerDocs);
+console.log(swaggerDocs2);
 
 export default swaggerDocs;
