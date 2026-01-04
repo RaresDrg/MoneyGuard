8863b433e63800e9b3b4cf4f7615be19a003536d
RaresDrg <dragan_rares13@yahoo.com>
2026-01-04 07:44:33 +0200

    asfsaf
    asfasfaf

--- server/src/routes/swaggerDocs/index.ts
+++ server/src/routes/swaggerDocs/index.ts
@@ -4,13 +4,8 @@ import { IN_DEVELOPMENT } from "../../config/config-env.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const swaggerDocs = [`${__dirname}/*.js`];
-
-const swaggerDocs2 = IN_DEVELOPMENT
+const swaggerDocs = IN_DEVELOPMENT
   ? [`${__dirname}/*.ts`]
   : [`${__dirname}/*.js`];
-
-console.log(swaggerDocs);
-console.log(swaggerDocs2);
 
 export default swaggerDocs;
